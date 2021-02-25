8c4be288d3eba6014acbf20dc545f34252d9796c
majki <michofilip@gmail.com>
2021-02-25 20:12:48 +0100

    new tables

--- conf/evolutions/mangas_db/002.sql
+++ conf/evolutions/mangas_db/002.sql
@@ -1,41 +1,22 @@
 -- !Ups
 
-INSERT INTO mangas(title)
-VALUES ('KonoSuba');
+CREATE TABLE IF NOT EXISTS collection
+(
+    id      SERIAL PRIMARY KEY,
+    user_id INT NOT NULL
+);
 
-INSERT INTO chapters(number, sub_number, title, manga_id)
-VALUES (1, 1, 'This Self Proclaimed Goddess and Reincarnation in Another World!', 1),
-       (2, 1, 'Joy for This Beautiful Upper Class Job!', 1),
-       (3, 1, 'Peace for These Undead! ', 1);
+CREATE TABLE IF NOT EXISTS collection_manga
+(
+    collection_id INT NOT NULL,
+    manga_id      INT NOT NULL,
 
-INSERT INTO pages(key, chapter_id, page_nr)
-VALUES ('page_1', 1, 1),
-       ('page_2', 1, 2),
-       ('page_3', 1, 3),
-       ('page_4', 2, 1),
-       ('page_5', 2, 2),
-       ('page_6', 2, 3);
+    CONSTRAINT collection_fk FOREIGN KEY (collection_id) REFERENCES collection (id),
+    CONSTRAINT manga_fk FOREIGN KEY (manga_id) REFERENCES mangas (id)
+);
 
-INSERT INTO genres(name)
-VALUES ('Harem'),
-       ('Isekai');
-
-INSERT INTO manga_genres(manga_id, genre_id)
-VALUES (1, 1),
-       (1, 2);
-
-INSERT INTO franchises(name)
-VALUES ('KonoSuba');
-
-INSERT INTO manga_franchises(manga_id, franchise_id)
-VALUES (1, 1);
 
 -- !Downs
 
-TRUNCATE TABLE mangas CASCADE;
-TRUNCATE TABLE chapters CASCADE;
-TRUNCATE TABLE pages CASCADE;
-TRUNCATE TABLE genres CASCADE;
-TRUNCATE TABLE manga_genres CASCADE;
-TRUNCATE TABLE franchises CASCADE;
-TRUNCATE TABLE manga_franchises CASCADE;
+DROP TABLE IF EXISTS collection CASCADE;
+DROP TABLE IF EXISTS collection_manga CASCADE;
