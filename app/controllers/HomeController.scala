1e024896414b0a397917619b181d10040f6aa5c1
majki <michofilip@gmail.com>
2021-02-27 23:03:56 +0100

    temporary fixes

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,8 +1,8 @@
 package controllers
 
-import javax.inject._
-import play.api._
 import play.api.mvc._
+
+import javax.inject._
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
@@ -11,14 +11,15 @@ import play.api.mvc._
 @Singleton
 class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
-  }
+    /**
+     * Create an Action to render an HTML page.
+     *
+     * The configuration in the `routes` file means that this method
+     * will be called when the application receives a `GET` request with
+     * a path of `/`.
+     */
+    def index() = Action { implicit request: Request[AnyContent] =>
+        Redirect(routes.MangaController.findAll())
+        //    Ok(views.html.index())
+    }
 }
